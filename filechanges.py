dcf3b2352736769db485c36475d576fa4aba6046
Adil Hasan <adilhasan2@gmail.com>
2021-07-11 22:49:51 +0100

    Added more code. adil

--- filechanges.py
+++ filechanges.py
@@ -3,6 +3,11 @@ import sys
 import sqlite3
 import optparse
 import hashlib
+import socket
+
+from datetime import datetime
+from openpyxl import Workbook
+from openpyxl.styles import Font
 
 # NOTE: This code is not really mine. It is taken from the solution from the manning liveproject
 
@@ -204,9 +209,10 @@ def md5_short(fname):
     return md5
 
 
-def load_fields(config):
+def load_fields():
     flds = []
     ext = []
+    config = "./filechanges.ini"
     if os.path.isfile(config):
         cfile = open(config, 'r')
         # Parse each config file line and get the folder and extensions
@@ -246,11 +252,57 @@ def runfilechanges(ws):
 
 
 def startxlsreport():
-    pass
+    wb = Workbook()
+    ws = wb.active
+    ws.title = socket.gethostname()
+    st = getdt("%d-%b-%Y %H_%M_%S")
+    return wb, ws, st
 
+def getdt(fmt):
+    today = datetime.today()
+    return today.strftime(fmt)
 
 def endxlsreport(wb, st):
-    pass
+    dt = ' from ' + st + ' to ' + getdt("%d-%b-%Y %H_%M_%S")
+    fn = "testfile" + dt + '.xlsx'
+    wb.save(fn)
+
+def headerxlsreport(ws):
+    ws.cell(row=1, column=1, value="File Name")
+    ws.cell(row=1, column=2, value="Full File Name")
+    ws.cell(row=1, column=3, value="Folder Name")
+    ws.cell(row=1, column=4, value="Date")
+    ws.cell(row=1, column=5, value="Time")
+
+    a1 = ws['A1']
+    b1 = ws['B1']
+    c1 = ws['C1']
+    d1 = ws['D1']
+    e1 = ws['E1']
+
+    a1.font = Font(color="000000", bold=True)
+    b1.font = Font(color="000000", bold=True)
+    c1.font = Font(color="000000", bold=True)
+    d1.font = Font(color="000000", bold=True)
+    e1.font = Font(color="000000", bold=True)
+
+def getlastrow(ws):
+    rw = 1
+    for cell in ws["A"]:
+        if cell.value is None:
+            break
+        else:
+            rw += 1
+    return rw
+
+def rowxlsreport(ws, fn, ffn, fld, d, t):
+    rw = getlastrow(ws)
+
+    ws.cell(row=rw, column=1, value=fn)
+    ws.cell(row=rw, column=2, value=ffn)
+    ws.cell(row=rw, column=3, value=fld)
+    ws.cell(row=rw, column=4, value=d)
+    ws.cell(row=rw, column=5, value=t)
 
 
 def execute(args):
